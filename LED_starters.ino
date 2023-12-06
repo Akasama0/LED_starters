18cc96aec774c9016f6d40d047fe78990d4a4e74
53175DDD <143765653+53175ddd@users.noreply.github.com>
2023-12-06 09:15:56 +0900

    add pin define and replace

--- LED_starters.ino
+++ LED_starters.ino
@@ -1,34 +1,38 @@
+#define LED_A_G 26
+#define LED_A_Y 27
+#define LED_A_R 28
+#define LED_B_G 29
+#define LED_B_Y  6
+#define LED_B_R  7
+
 void setup() {
-  pinMode(26,OUTPUT);
-  pinMode(27,OUTPUT);
-  pinMode(28,OUTPUT);
-  pinMode(29,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
+  pinMode(LED_A_G, OUTPUT);
+  pinMode(LED_A_Y, OUTPUT);
+  pinMode(LED_A_R, OUTPUT);
+  pinMode(LED_B_G, OUTPUT);
+  pinMode(LED_B_Y, OUTPUT);
+  pinMode(LED_B_R, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.print("start\n");
-  digitalWrite(26,HIGH);
+  digitalWrite(LED_A_G, HIGH);
   delay(500);
-  digitalWrite(26,LOW);
-  digitalWrite(27,HIGH);
+  digitalWrite(LED_A_G,  LOW);
+  digitalWrite(LED_A_Y, HIGH);
   delay(500);
-  digitalWrite(27,LOW);
-  digitalWrite(28,HIGH);
+  digitalWrite(LED_A_Y,  LOW);
+  digitalWrite(LED_A_R, HIGH);
   delay(500);
-  digitalWrite(28,LOW);
-  digitalWrite(28,HIGH);
+  digitalWrite(LED_A_R,  LOW);
+  digitalWrite(LED_B_G, HIGH);
   delay(500);
-  digitalWrite(28,LOW);
-  digitalWrite(29,HIGH);
+  digitalWrite(LED_B_G,  LOW);
+  digitalWrite(LED_B_Y, HIGH);
   delay(500);
-  digitalWrite(29,LOW);
-  digitalWrite(6,HIGH);
+  digitalWrite(LED_B_Y,  LOW);
+  digitalWrite(LED_B_R, HIGH);
   delay(500);
-  digitalWrite(6,LOW);
-  digitalWrite(7,HIGH);
-  delay(500);
-  digitalWrite(7,LOW);
+  digitalWrite(LED_B_R,  LOW);
 }
